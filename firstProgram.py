411aa956f1915fe8a15518b3530b214b085ec475
Sonny klutsey <sonnyklutseyk@gmail.com>
2025-01-19 13:29:28 +0800

    firstprogram updated

---
+++ firstProgram.py
@@ -0,0 +1,10 @@
+ # print a greeting
+print("Hello! Welocome to programming")
+# Ask of name
+name = input("What is your name? ") # name = "Sonny" 
+print(name)
+# Ask where he was born
+city = input("Which city were you born? ") # city = "Accra Ghana"
+print(city)
+# Create a message by combining strings and the information stored in variables
+print(f'Hello, {name}. I have heard that {city} is a facinitation place! ')
